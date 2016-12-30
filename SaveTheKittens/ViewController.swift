20196a07bad376814f3b3e613f8a3be679741c3b
RapGodGabe <gabriel_lee@Gabriels-iMac.local>
2016-12-30 18:11:17 -0500

    Lol again
    Lol again and again

--- SaveTheKittens/ViewController.swift
+++ SaveTheKittens/ViewController.swift
@@ -15,8 +15,13 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         
         print("Hello yoooo")
+        
+        print("yarn yarn")
+        
+         print("yeoooo")
     }
 
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
